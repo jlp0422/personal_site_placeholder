c47ad1a2487e4130f68d2e6b565427ef2ea3e5fa
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-04-27 18:26:43 -0400

    chore: site under construction

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,14 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#1f375b`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
+        textAlign: 'center',
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ margin: '0 auto', textAlign: 'center'}}>
+      <h2>Site under construction</h2>
+      <div style={{ padding: '50px' }}>
+        <span role="img" aria-label="consturction" style={{ fontSize: '120px' }}>🚧</span>
+      </div>
+      <p>Come back soon.</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
